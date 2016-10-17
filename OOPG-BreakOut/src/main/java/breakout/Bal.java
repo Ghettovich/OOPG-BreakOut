546c74dd565ae175a599b91c991e38f5c24d1597
Ghettovich <geert.boeve92@gmail.com>
2016-10-17 20:35:56 +0200

    breakout start-up klasse toegevoegd. Alleen een andere achtergrond.

---
+++ OOPG-BreakOut/src/main/java/breakout/Bal.java
@@ -0,0 +1,34 @@
+package breakout;
+
+import java.util.List;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.CollidedTile;
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithGameObjects;
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithTiles;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
+import nl.han.ica.OOPDProcessingEngineHAN.Sound.Sound;
+import processing.core.PGraphics;
+
+public class Bal extends GameObject implements ICollidableWithTiles {
+
+	
+	
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(PGraphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tileCollisionOccurred(List<CollidedTile> collidedTiles) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ OOPG-BreakOut/src/main/java/breakout/BreakOut.java
@@ -0,0 +1,85 @@
+package breakout;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Dashboard.Dashboard;
+import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+import nl.han.ica.OOPDProcessingEngineHAN.Persistence.FilePersistence;
+import nl.han.ica.OOPDProcessingEngineHAN.Persistence.IPersistence;
+import nl.han.ica.OOPDProcessingEngineHAN.Sound.Sound;
+import nl.han.ica.OOPDProcessingEngineHAN.Tile.TileMap;
+import nl.han.ica.OOPDProcessingEngineHAN.Tile.TileType;
+import nl.han.ica.OOPDProcessingEngineHAN.View.EdgeFollowingViewport;
+import nl.han.ica.OOPDProcessingEngineHAN.View.View;
+import nl.han.ica.waterworld.TextObject;
+import nl.han.ica.waterworld.tiles.BoardsTile;
+import processing.core.PApplet;
+
+
+public class BreakOut extends GameEngine {
+	
+	private TextObject dashboardText;
+	private Bal bal;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main(new String[]{"breakout.BreakOut"});
+	}
+
+    /**
+     * In deze methode worden de voor het spel
+     * noodzakelijke zaken geïnitialiseerd
+     */
+	@Override
+	public void setupGame() {
+		// TODO Auto-generated method stub
+        int worldWidth=1024;
+        int worldHeight=768;
+        
+        //initializeSound();
+        createDashboard(worldWidth, 100);
+        
+        //initializeTileMap();
+        //initializePersistence();
+
+        createObjects();
+        createViewWithoutViewport(worldWidth, worldHeight);
+        //createViewWithViewport(worldWidth, worldHeight, 800, 800, 1.1f);
+		
+	}
+	
+    /**
+     * Creeërt de view zonder viewport
+     * @param screenWidth Breedte van het scherm
+     * @param screenHeight Hoogte van het scherm
+     */
+    private void createViewWithoutViewport(int screenWidth, int screenHeight) {
+        View view = new View(screenWidth,screenHeight);
+        view.setBackground(loadImage("src/main/java/breakout/media/Penguins.jpg"));
+
+        setView(view);
+        size(screenWidth, screenHeight);
+    }
+	
+    private void createObjects() {
+		// TODO Auto-generated method stub
+    	
+    	bal = new Bal();
+    	this.addGameObject(bal);
+    	
+		System.out.println("sup with it");
+	}
+
+	private void createDashboard(int dashboardWidth,int dashboardHeight) {
+        Dashboard dashboard = new Dashboard(0,0, dashboardWidth, dashboardHeight);
+        dashboardText=new TextObject("");
+        dashboard.addGameObject(dashboardText);
+        addDashboard(dashboard);
+    }
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
